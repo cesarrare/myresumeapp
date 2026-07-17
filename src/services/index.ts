956c18a5f141d4389c1c4bc1bd08b0c43388eaa7
cesarrare <cesar.rare@gmail.com>
2026-07-17 17:51:32 -0600

    fix(auth) : fixed faulty merge in auth files

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-export { getDefaultResume, getResumeById, getResumesByUserId, saveResume, updateResume } from './resumeService'
-export { login, register } from './authService'
-=======
 export { getDefaultResume, getResumeById, getResumesByUserId, saveResume, updateResume } from './resumeService'
 export { login, loginWithGoogle, register } from './authService'
->>>>>>> Stashed changes
